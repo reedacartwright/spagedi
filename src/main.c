2964d0200722c2f89855e8eb7af75a46510cbe7b
Reed Cartwright <cartwright@asu.edu>
2009-02-28 05:25:46 +0000

    restored wait_a_char at end of run

--- src/main.c
+++ src/main.c
@@ -228,7 +228,7 @@ strcpy(instrfile,"instruction.txt");
 	sprintf(smess,"\n\n ");
 	write_tofile_only(outputfile,smess);
 	printf("\n\nThe program has finished normally. \nLet's now open the file %c%s%c with a worksheet program\n... and digest SPAGeDi's results. \nPress any key to close this program.\n",'"',outputfile,'"');
-//	if(argc<4) wait_a_char();			
+	if(argc<4) wait_a_char();			
 	
 	return 0;
 }	//end of main
