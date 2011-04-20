eb0f54126c697175706a4d4a22e34898d90a35af
Reed Cartwright <cartwright@asu.edu>
2011-04-20 22:59:45 +0000

    added mingw32 support

--- releng/build-rel-unix.sh
+++ releng/build-rel-unix.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+PROJ=spagedi
+PROJ_DISTS=spagedi-1*
 MAKE=make
 CMAKE=cmake
-REPOS=svn://scit.us/documents/projects/spagedi
+REPOS=`svn info | grep URL: | perl -pe 's!^URL: (.+)/releng$!$1!'`
 
-RELENG_DIR=`mktemp -q -d -t spagedi-releng.XXX`
+build_mingw32=
+build_m32=
+for build_option; do
+	case $build_option in
+	--mingw32)
+		build_mingw32=yes ;;
+	--m32)
+		build_m32=yes ;;
+	esac
+done
+
+RELENG_DIR=`mktemp -q -d -t ${PROJ}-releng.XXX`
 if [ $? -ne 0 ]; then
         echo "$0: Can't create temp directory, exiting..."
         exit 1
@@ -19,12 +32,32 @@ svn co -q $REPOS $SOURCE_DIR || exit 1
 mkdir $BUILD_DIR || exit 1
 cd $BUILD_DIR || exit 1
 
-$CMAKE $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release
+if test -n "build_mingw32"; then
+	$CMAKE $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_TOOLCHAIN_FILE="${SOURCE_DIR}/releng/i586-mingw32msvc.cmake" \
+		-DUSE_STATIC_LIBS=on
+elif test -n "build_m32"; then
+	$CMAKE $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release
+else
+	$CMAKE $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release \
+		-DBoost_USE_STATIC_LIBS=ON -DGSL_USE_STATIC_LIBS=ON
+fi
 $MAKE
 $MAKE package
 $MAKE package_source
 
-mv SPAGeDi-1* $DEST_DIR
+
+
+cd $DEST_DIR
+rm -rf $RELENG_DIR
+
+echo
+echo Moving distribution packages ...
+
+mv -v SPAGeDi-1* $DEST_DIR
+
+echo
+echo Cleaning up ...
 
 cd $DEST_DIR
 rm -rf $RELENG_DIR
