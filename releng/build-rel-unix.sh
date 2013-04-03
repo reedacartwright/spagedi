ddedd1dd4acf6b11e05095281f9624fd85aa0823
reedacartwright <cartwright@asu.edu>
2013-04-03 13:21:50 -0700

    Update unix build script to call cmake script

--- releng/build-rel-unix.sh
+++ releng/build-rel-unix.sh
@@ -1,102 +1,33 @@
 #!/bin/sh
 # Download and build a package from GitHub
 
-PROJ=spagedi
-PROJ_DISTS=SPAGeDi-1*
-GH=https://github.com/reedacartwright/spagedi/archive/
-MAKE=make
 CMAKE=cmake
 
-build_mingw32=
-build_m32=
-build_archive=HEAD
-for build_option; do
-	case $build_option in
-	--mingw32)
-		build_mingw32=yes ;;
-	--m32)
-		build_m32=yes ;;
-	*)
-		build_archive=$build_option ;;
+# Process command line arguments
+TEMP=`getopt -o t: --long toolchain: -n $0 -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+build_toolchain=
+while true ; do
+	case "$1" in
+		-t|--toolchain) build_toolchain=$2 ; shift 2 ;;
+		--) shift ; break ;;
+		*) echo "Internal error!" ; exit 1 ;;
 	esac
 done
 
-RELENG_DIR=`mktemp -q -d -t ${PROJ}-releng.XXX`
+# Determine the archive tag
+build_archive=${1-HEAD}
+build_args="-DRELENG_TAG=${build_archive}"
 
-echo Using $RELENG_DIR as a temporary build directory ...
-
-if [ $? -ne 0 ]; then
-        echo "$0: Can't create temp directory, exiting..."
-        exit 1
+# if toolchain is m32 or m64 use the flags and not a toolchain
+if [ -n "${build_toolchain}" ]; then
+	case "${build_toolchain}" in
+		m32|M32) build_args="${build_args} -DRELENG_M32=on" ;;
+		m64|M64) build_args="${build_args} -DRELENG_M64=on" ;;
+		*)       build_args="${build_args} -DRELENG_TOOLCHAIN=${build_toolchain}" ;;
+	esac
 fi
 
-DEST_DIR=`pwd`
-cd $RELENG_DIR || exit 1
-
-archive=${PROJ}.tar.gz
-
-echo 
-echo Downloading SPAGeDi Archive from GitHub ...
-
-wget -nv -O ${archive} "${GH}${build_archive}.tar.gz" || exit 1
-
-
-echo 
-echo Extracting Archive ...
-
-tar xzf ${archive} && cd ${PROJ}-* || exit 1
-
-echo 
-echo Identifying Archive ...
-
-version=`pwd | sed "s/^.*\/${PROJ}-\(.*\)\$/\1/"`
-version_len=`expr $version : '.*'`
-
-if [ $version_len -eq 40 ]; then
-	version=`echo $version | cut -c1-7`
-	version=`grep "# Default Version Number" CMakeLists.txt | sed -e 's/^.*"\(.*\)".*$/\1/' -e "s/CUSTOM/g$version/"`
-	echo "    Archive" is commit $version.
-else
-	echo "    Archive" is tag $version.
-fi
-
-echo
-echo Configuring Source ...
-
-mkdir -p build && cd build
-
-if test $build_mingw32; then
-	$CMAKE .. -DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_TOOLCHAIN_FILE="../releng/mingw32.cmake" \
-		-DUSE_STATIC_LIBS=on \
-		-DSPAGEDI_VERSION="${version}"
-elif test $build_m32; then
-	$CMAKE .. -DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_C_FLAGS=-m32 \
-		-DUSE_STATIC_LIBS=on \
-		-DSPAGEDI_VERSION="${version}"
-else
-	$CMAKE .. -DCMAKE_BUILD_TYPE=Release \
-	-DUSE_STATIC_LIBS=on \
-	-DSPAGEDI_VERSION="${version}"
-fi
-
-echo
-echo Building Binary Package ...
-$MAKE new_package || exit 1
-
-echo
-echo Build Source Package ...
-$MAKE new_package_source || exit 1
-
-echo
-echo Moving distribution packages ...
-
-mv -v ${PROJ_DISTS} ${DEST_DIR}
-
-echo
-echo Cleaning up ...
-
-cd $DEST_DIR
-rm -rf $RELENG_DIR
+$CMAKE $build_args -P releng.cmake
 
